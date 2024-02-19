c100be884b879ae947e92d3a543c6457a0f072c0
Aman Brar <aman1531@gmail.com>
2024-02-19 15:12:57 +0530

    Modal added to store item in localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,28 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+const input = document.getElementById("endDate");
+const today = new Date();
+
+// Format the date as YYYY-MM-DD
+const dateString = today.toISOString().split('T')[0];
+
+// Set the value of the input element
+input.value = dateString;
+
+const itemAdd = document.getElementById('itemAdd');
+
+itemAdd.addEventListener("click", function(e){
+    localStorage.clear();
+
+    const item = {
+title:document.getElementById("title").value,
+description: document.getElementById("desc").value,
+enddate:document.getElementById('endDate').value,
+priority:document.getElementById('priority').value,
+status:"To Do"
+}
+localStorage.setItem("db",JSON.stringify(item))
+console.log(localStorage.getItem("db"))
+});
