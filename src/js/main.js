3e3e217c0fcee13827977a6824c3493b68a1ff04
Aman Brar <aman1531@gmail.com>
2024-02-19 16:33:10 +0530

    three lists displayed based on status

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+var db = JSON.parse(localStorage.getItem("db"))
+
 const input = document.getElementById("endDate");
 const today = new Date();
 
@@ -15,16 +17,36 @@ input.value = dateString;
 
 const itemAdd = document.getElementById('itemAdd');
 
-itemAdd.addEventListener("click", function(e){
-    localStorage.clear();
+function displayLists() {
+var ul = document.getElementById("todoList")
+db.map((item) => {
+    var li = document.createElement("li");
 
+if (item.status == "To Do") { 
+li.appendChild(document.createTextNode(item.title));
+ul.appendChild(li);
+}
+});
+};
+window.onload = displayLists()
+itemAdd.addEventListener("click", function(e){
+    
+    let index = 0
+    if(db)
+    index = db.length
     const item = {
+        index: index,
 title:document.getElementById("title").value,
 description: document.getElementById("desc").value,
 enddate:document.getElementById('endDate').value,
 priority:document.getElementById('priority').value,
 status:"To Do"
 }
-localStorage.setItem("db",JSON.stringify(item))
+if(index)
+db.push(item)
+else
+db = [item]
+
+localStorage.setItem("db",JSON.stringify(db))
 console.log(localStorage.getItem("db"))
 });
